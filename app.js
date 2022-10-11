5a13c3ed37f1a38b7363ee603750c8c4875d63b9
immThnh <103478846+immThnh@users.noreply.github.com>
2022-10-12 02:12:36 +0700

    upload modal btn

--- app.js
+++ app.js
@@ -1,5 +1,67 @@
 $(document).ready(function(){
+  $('.single-item').slick()
   $('.body__event-load').slick({
-        slidesToShow: 3,
+        slidesToShow: 1,
+        autoplay:true,
+        autoplaySpeed: 2000,
+        draggable: false,
+        dots: true,
+        arrows: true,
+        prevArrow:"<button type='button' class='slick-prev pull-left btn-pagination-icon'><i class='ti-angle-left body__pagination-arrow' aria-hidden='true'></i></button>",
+        nextArrow:"<button type='button' class='slick-next pull-right btn-pagination-icon'><i class='ti-angle-right body__pagination-arrow' aria-hidden='true'></i></button>",
+        pauseOnDotsHover: true,
   });
+  $('.event-info').slick({
+        slidesToShow: 3,
+        arrows: true,
+        infinite: false,
+        prevArrow:"<button type='button' class='slick-prev pull-left btn-pagination-icon'><i class='ti-angle-left body__pagination-arrow' aria-hidden='true'></i></button>",
+        nextArrow:"<button type='button' class='slick-next pull-right btn-pagination-icon'><i class='ti-angle-right body__pagination-arrow' aria-hidden='true'></i></button>",
+        // responsive: [
+        //   {
+        //     breakpoint: 1830,
+        //     settings: {
+        //       slidesToShow: 3,
+        //       slidesToScroll: 3,
+        //       infinite: true,
+        //       dots: true
+        //     }
+        //   },
+        //   {
+        //     breakpoint: 1024,
+        //     settings: {
+        //       slidesToShow: 3,
+        //       slidesToScroll: 3,
+        //       infinite: true,
+        //       dots: true
+        //     }
+        //   },
+        //   {
+        //     breakpoint: 600,
+        //     settings: {
+        //       slidesToShow: 2,
+        //       slidesToScroll: 2
+        //     }
+        //   },
+        //   {
+        //     breakpoint: 480,
+        //     settings: {
+        //       slidesToShow: 1,
+        //       slidesToScroll: 1
+        //     }
+        //   }
+        //   // You can unslick at a given breakpoint now by adding:
+        //   // settings: "unslick"
+        //   // instead of a settings object
+        // ]
+  });
+
+  $('.body__game-cart').slick({
+    slidesToShow: 8,
+    arrow: true,
+    infinite: false,
+    prevArrow:"<button type='button' class='slick-prev pull-left btn-pagination-icon'><i class='ti-angle-left body__pagination-arrow' aria-hidden='true'></i></button>",
+    nextArrow:"<button type='button' class='slick-next pull-right btn-pagination-icon'><i class='ti-angle-right body__pagination-arrow' aria-hidden='true'></i></button>",
+  }) 
 });
+
